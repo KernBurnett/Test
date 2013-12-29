cdec5560bcae6a3feea8cac60c4aa19bf601d0fe
Calvin Kern <calvinmkern@gmail.com>
2013-12-29 02:15:16 -0700

    Added a comment to main application (for testing commiting)

--- Test Application/src/main/java/com/kernburnett/testapplication/MainActivity.java
+++ Test Application/src/main/java/com/kernburnett/testapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.kernburnett.testapplication;
 
+// Test application for Kern Burnett Git repository
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
