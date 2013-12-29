17973f43387229cee58b35415c3f9525938d5095
Calvin Kern <calvinmkern@gmail.com>
2013-12-29 02:55:53 -0700

    Added comments to TestView describing what it's future holds

--- Test Application/src/main/java/com/kernburnett/testapplication/TestView.java
+++ Test Application/src/main/java/com/kernburnett/testapplication/TestView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.View;
 
 /**
+ * This is an empty class for testing the ability to push changes to Git
  * Created by Calvin on 12/29/13.
  */
 public class TestView extends View
