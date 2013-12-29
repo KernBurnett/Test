2a6a46058bbeabbae54a12e01d0271c6a4f18f47
Calvin Kern <calvinmkern@gmail.com>
2013-12-29 02:53:43 -0700

    Created a test class that extends view.

---
+++ Test Application/src/main/java/com/kernburnett/testapplication/TestView.java
@@ -0,0 +1,16 @@
+package com.kernburnett.testapplication;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Calvin on 12/29/13.
+ */
+public class TestView extends View
+{
+
+    public TestView(Context context)
+    {
+        super(context);
+    }
+}
